e21be0a4a345bc4cde9c9f279c8d886802f15e74
jucandoit <ods630805@empas.com>
2019-04-06 20:37:52 +0900

    투두리스트, 배경 , css 수정

--- javascript/momentumApp/js/todo.js
+++ javascript/momentumApp/js/todo.js
@@ -1,18 +1,65 @@
-const todoForm = document.querySelector('.todoForm');
-const todoInput = form.querySelector('input');
-const todoList = document.querySelector('.todoList');
+const todoForm = document.querySelector('.toDoForm');
+const todoInput = todoForm.querySelector('input');
+const todoList = document.querySelector('.toDoList');
 
 const TODOS_LS = 'toDos';
+
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const list = btn.parentNode;
+  todoList.removeChild(list);
+
+  const cleanToDos = toDos.filter(function(toDo) {
+    console.log(toDo.id, parseInt(list.id));
+    return toDo.id !== parseInt(list.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const delBtn = document.createElement('button');
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌"
+  delBtn.addEventListener('click', deleteToDo);
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  todoList.appendChild(li);
+
+  span.innerText = text;
+
+  const toDoObj = {
+    text: text,
+    id: newId
+  }
+
+  toDos.push(toDoObj);
+  saveToDos();
+}
 
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = todoInput.value;
+  paintToDo(currentValue);
+  todoInput.value = "";
 }
 
 function loadTodos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
-
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
