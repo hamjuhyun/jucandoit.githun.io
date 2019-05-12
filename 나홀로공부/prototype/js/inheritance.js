fcbbaa3d0cf2b806d01327a3124211f41a4a9e6b
jucandoit <ods630805@empas.com>
2019-05-12 14:19:17 +0900

    npm 설치

---
+++ 나홀로공부/prototype/js/inheritance.js
@@ -0,0 +1,32 @@
+class Person {
+  constructor(name, first, second) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+  }
+
+  sum() {
+    return this.first + this.second;
+  }
+}
+
+class PersonPlus extends Person {
+  constructor(name, first, second, third) {
+    super(name, first, second);
+    this.third = third;
+  }
+
+  sum() {
+    return super.sum()  + this.third;
+  }
+
+  avg(n = 3) {
+    return (this.first + this.second + this.third) / n;
+  }
+}
+
+var kim = new PersonPlus('kim', 10, 20, 30);
+console.log("kim.sum()", kim.sum());
+console.log("kim.avg()", kim.avg());
+
+
